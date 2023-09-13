c15f0e2ea71fae22715f4a214614be5416668fab
rsammer07 <ryan.sammer07@gmail.com>
2023-09-12 20:31:45 -0700

    added functions for correct answer, wrong answer, hideElements and displayQuestion

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-//-----------constants-----------//
-const optionOne = document.querySelector(`#buttonOne`)
-const optionTwo = document.querySelector(`#buttonTwo`)
-const optionThree = document.querySelector(`#buttonThree`)
-const optionFour = document.querySelector(`#buttonFour`)
-const nextButton = document.querySelector(`#nextButton`)
+//--------cached elements--------//
+let question = document.querySelector(`#question`)
+let buttonContainer = document.querySelector(`#buttonContainer`)
+let scoreValue = document.querySelector(`#scoreValue`)
+let optionOne = document.querySelector(`#buttonOne`)
+let optionTwo = document.querySelector(`#buttonTwo`)
+let optionThree = document.querySelector(`#buttonThree`)
+let optionFour = document.querySelector(`#buttonFour`)
+let nextButton = document.querySelector(`#nextButton`)
+let starImage = document.querySelector(`#starImage`)
 
+buttonContainer.style.visibility = `hidden`;
+//-----------constants-----------//
 const choices = [optionOne, optionTwo, optionThree, optionFour]
 
 const questions = [
     {
-        questionText: `Who shot first`,
+        questionText: `Who shot first?`,
         choices: [`Han Solo`, `Luke Skywalker`, `Greedo`, `R2-D2`],
         correctAnswer: `Han Solo`
     },
@@ -49,24 +55,18 @@ const questions = [
         correctAnswer: `Jango Fett`,
     },
     {
-        questionText: ``,
-        choices: [],
-        correctAnswer: ``,
+        questionText: `Who is the Senate?`,
+        choices: [`Boba Fett`, `Senator Palpatine`, `Bail Organa`, `Queen Amidala`],
+        correctAnswer: `Senator Palpatine`,
     },
     {
-        questionText: ``,
-        choices: [],
-        correctAnswer: ``,
+        questionText: `Who unleashed the Rancor to kill Luke Skywalker?`,
+        choices: [`Jabba the Hutt`, `Emporer Palpatine`, `Old Ben Kanobi`, `Yoda`],
+        correctAnswer: `Jabba the Hutt`,
     },
 ]
 
 //-----------variables-----------//
-let question = document.querySelector(`#question`)
-let buttonContainer = document.querySelector(`#buttonContainer`)
-let scoreValue = document.querySelector(`#scoreValue`)
-
-
-//--------cached elements--------//
 
 
 
@@ -76,6 +76,47 @@ let scoreValue = document.querySelector(`#scoreValue`)
 
 
 //----------functions----------//
+//establish a wrongAnswer function
 wrongAnswer = function() {
-    main.style.backgroundImage = `url("./Darth-Vader.png")`
-}
+        //I want to change what is displayed on the screen after question is answered.
+        main.style.backgroundImage = `url("./Photos/Darth-Vader.png")`;
+        nextButton.style.visibility = `visible`;
+        hideElements();
+};
+
+
+correctAnswer = function() {
+        scoreValue = score + 1;
+        main.style.backgroundImage = `url("./Photos/Yoda.jpeg)`;
+        hideElements();
+};
+
+
+hideElements = function() {
+    question.style.visibility = `hidden`;
+    buttonContainer.style.visibility = `hidden`;
+    starImage.style.visibility = `hidden`;
+};
+
+// hideElements();
+//set current index of question to 0 so it begins from the first question
+let currentQuestionIndex = 0;
+
+//create a displayQuestion function
+displayQuestion = function() {
+    //I want to hide the `startButton`
+    startButton.style.visibility = `hidden`;
+    //I want to bring my buttonContainer back in view
+    buttonContainer.style.visibility = `visible`;
+    //tell code what current index we are currently on
+  const currentQuestion = questions[currentQuestionIndex];
+    //change innerHTML of question to the new question
+  question.textContent = currentQuestion.questionText;
+    //change innerHTML of choices to the choices value I have in the array of questions
+  for (let i = 0; i < choices.length; i++) {
+    choices[i].textContent = currentQuestion.choices[i];
+  };
+};
+
+// wrongAnswer();
+displayQuestion();
