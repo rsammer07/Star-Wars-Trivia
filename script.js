eabcd48891979cd7943e7aaf55e4c515d4ad1bb2
rsammer07 <ryan.sammer07@gmail.com>
2023-09-19 17:46:55 -0700

    MVP commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//--------cached elements--------//
 let question = document.querySelector(`#question`);
 let buttonContainer = document.querySelector(`#buttonContainer`);
 let scoreDisplay = document.querySelector(`#scoreValue`);
@@ -9,11 +8,8 @@ let optionFour = document.querySelector(`#buttonFour`);
 let nextButton = document.querySelector(`#nextButton`);
 let starImage = document.querySelector(`#starImage`);
 let startButton = document.querySelector(`#startButton`);
-
 buttonContainer.style.visibility = `hidden`;
-//-----------constants-----------//
 const choices = [optionOne, optionTwo, optionThree, optionFour];
-
 const questions = [
     {
         questionText: `Who shot first?`,
@@ -66,36 +62,24 @@ const questions = [
         correctAnswer: `Jabba the Hutt`,
     },
 ];
-
-//-----------variables-----------//
 let scoreValue = 0;
 scoreDisplay.innerHTML = scoreValue;
-
-//--------event listeners-------//
-//I need to establish an event listener that will start the quiz from the startButton
 startButton.addEventListener(`click`, (e) => {
     e.preventDefault();
     displayQuestion();
     
 });
-//I want event listners on all the choice buttons so I can invoke the checkAnswer function.
 for (const choice of choices) {
     choice.addEventListener(`click`, (e) => {
         e.preventDefault();
         checkAnswer(choice.textContent)
     });
 };
-//Now I need to establish an event listner to the nextButton
 nextButton.addEventListener(`click`, (e) => {
     e.preventDefault();
     nextQuestion();
 });
-
-
-//----------functions----------//
-//establish a wrongAnswer function
 wrongAnswer = function() {
-        //I want to change what is displayed on the screen after question is answered.
         main.style.backgroundImage = `url("./Photos/Darth-Vader.png")`;
         nextButton.style.visibility = `visible`;
         hideElements();
@@ -109,55 +93,36 @@ correctAnswer = function() {
         nextButton.style.visibility = `visible`;
         hideElements();
 };
-
-//I want to hide all the elements on the page to make way for my correct and incorrect functions to change the background image.
 hideElements = function() {
-    //hiding the question
     question.style.visibility = `hidden`;
-    //hiding the buttons
     buttonContainer.style.visibility = `hidden`;
-    //hiding the star wars logo
     starImage.style.visibility = `hidden`;
 };
-//I want to do the opposite of all my hideElements functions so the screen goes back to the next question.
 showElements = function() {
     question.style.visibility = `visible`;
     buttonContainer.style.visibility = `visible`;
     starImage.style.visibility = `visible`;
     main.style.backgroundImage = `url("./Photos/Star\ Wars.jpg")`;
-}
-// hideElements();
-//set current index of question to 0 so it begins from the first question
+};
 let currentQuestionIndex = 0;
-
-//create a displayQuestion function
 displayQuestion = function() {
-    //I want to hide the `startButton`
     startButton.style.visibility = `hidden`;
-    //I want to bring my buttonContainer back in view
+    nextButton.style.visibility = `hidden`;
     buttonContainer.style.visibility = `visible`;
-    //tell code what current index we are currently on
   const currentQuestion = questions[currentQuestionIndex];
-    //change innerHTML of question to the new question
   question.textContent = currentQuestion.questionText;
-    //change innerHTML of choices to the choices value I have in the array of questions
   for (let i = 0; i < choices.length; i++) {
     choices[i].textContent = currentQuestion.choices[i];
   };
 };
-
-//I need a function that will check if the users answer is correct or incorrect
 checkAnswer = function(answer) {
     const currentQuestion = questions[currentQuestionIndex];
     if (answer === currentQuestion.correctAnswer) {
-        //If users answer is correct I will invoke the correctAnswer function
       correctAnswer();
     } else {
-        //If users answer is incorrect I will invoke the wrongAnswer function
       wrongAnswer();
     };
   };
-  // I need a function that will take me to the next question to invoke in my nextButton event listener.
 nextQuestion = function() {
     currentQuestionIndex++;
     if (currentQuestionIndex < questions.length) {
@@ -167,18 +132,14 @@ nextQuestion = function() {
         checkScore();
     };
 };
-//After user has answered all 10 questions I want the program to check users score and give them their final score screen.
 checkScore = function() {
         nextButton.style.visibility = `hidden`;
-        //if score is 0-4 user is a padawan learner
         if (scoreValue <= 4) {
             hideElements();
             main.style.backgroundImage = `url("./Photos/padawan.jpeg")`
-            //if score is 5-9 user are a jedi knight
         } if (scoreValue >= 5 && scoreValue <= 9) {
             hideElements();
             main.style.backgroundImage = `url("./Photos/jedi-knight.jpeg")`
-            //if score is 10/10 user is a Jedi Master
         } else if (scoreValue === 10) {
             hideElements();
             main.style.backgroundImage = `url("./Photos/jedi-master.jpeg")`
